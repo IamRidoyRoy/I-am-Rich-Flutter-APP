ae4341436b86d1ca7d7d7ad555c25575ffd13637
Ridoy Roy <ridoyroycse53engg@gmail.com>
2022-09-19 23:59:08 +0600

    Add a local image on my flutte app

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-      MaterialApp(
-        home: Center(
-          child: Text('I am Rich'),
-        ),
-      ),
-    );
+void main() => runApp(MaterialApp(
+    home: Scaffold(
+        backgroundColor: Color.fromARGB(255, 183, 192, 204),
+        appBar: AppBar(
+            title: Text("I'm Rich -- Flutter Rocks"),
+            backgroundColor: Color.fromARGB(255, 61, 113, 139)),
+        body: Center(
+            child: Image(
+          image: AssetImage("images/diamond.png"),
+          // image: NetworkImage(
+          //     'https://static.vecteezy.com/packs/media/vectors/term-bg-1-666de2d9.jpg')),
+        )))));
